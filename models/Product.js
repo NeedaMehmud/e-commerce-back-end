6691a498fc5979660192ab947b3cf9de16703475
Needa Mehmud <needa.mehmud@gmail.com>
2021-07-25 17:24:43 -0400

    added stock category

--- models/Product.js
+++ models/Product.js
@@ -4,13 +4,42 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    //==== Doesn't allow null value. Set as primary key. Uses auto increment. ====//
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    //==== Product name needs to be a string. Doesn't allow null values. ====//
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    //==== Decimal. Doesn't allow null values. Validates that the value is a decimal. ====//
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    //==== Integer. Doesn't allow null values. Set a default value of `10`. Validates that the value is numeric. ====//
+    stock:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    }
   },
+  
   {
     sequelize,
     timestamps: false,
