164c3e450b3333eb9372f031842284826bf3882b
Needa Mehmud <needa.mehmud@gmail.com>
2021-07-24 22:08:09 -0400

    added routes to index.js in api dir

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 const router = require('express').Router();
+const categoryRoutes = require('./api/category-routes');
+const productRoutes = require('./api/product-routes');
+const tagRoutes = require('./api/tag-routes');
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.use('/categories', categoryRoutes);
+router.use('/products', productRoutes);
+router.use('/tags', tagRoutes);
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
 });
 
 module.exports = router;
+
+
