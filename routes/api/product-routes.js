5d272cf06b2b07d38b830a9fdc7f121b7af16871
Needa Mehmud <needa.mehmud@gmail.com>
2021-07-25 21:52:50 -0400

    created new product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -49,7 +49,7 @@ router.get('/:id', (req, res) => {
   })
     .then(dbProductData => {
       if (!dbProductData) {
-        res.status(404).json({message: 'There was no product found with this id, try another product'});
+        res.status(404).json({ message: 'There was no product found with this id, try another product' });
         return;
       }
       res.json(dbProductData);
@@ -70,7 +70,14 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  // create new product
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: req.body.tagIds
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
